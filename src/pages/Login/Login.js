d57858a7132193770e87dc4490b4b2ddbdeaf208
Nacorga <ncg2392@gmail.com>
2020-12-07 20:51:50 +0100

    [FEAT] - Alert API messages

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -42,7 +42,7 @@ const LoginPage = () => {
       history.push("/users");
 
     } catch(err) {
-      console.log(err.message);
+      alert(err.message);
     }
 
   }
--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -55,7 +55,7 @@ const UsersPage = () => {
       setMemoUsers(data);
 
     } catch(err) {
-      console.log(err.message);
+      alert(err.message);
     }
 
   }
